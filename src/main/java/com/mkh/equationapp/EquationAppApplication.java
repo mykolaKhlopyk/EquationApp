9d1a67162ba48a45d88a26f511c9348aad912d98
mykola <mykola.khlopyk@gmail.com>
2023-06-13 22:10:35 +0300

    initial commit

---
+++ src/main/java/com/mkh/equationapp/EquationAppApplication.java
@@ -0,0 +1,13 @@
+package com.mkh.equationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquationAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EquationAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mkh/equationapp/EquationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkh.equationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquationAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
