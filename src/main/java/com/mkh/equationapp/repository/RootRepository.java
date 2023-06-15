bf628e6ad1d8f86298ccc67d8c0aeba861853e90
mykola <mykola.khlopyk@gmail.com>
2023-06-15 04:38:03 +0300

    small update

--- src/main/java/com/mkh/equationapp/repository/RootRepository.java
+++ src/main/java/com/mkh/equationapp/repository/RootRepository.java
@@ -2,6 +2,8 @@ package com.mkh.equationapp.repository;
 
 import com.mkh.equationapp.domain.Root;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RootRepository extends JpaRepository<Root, Long> {
 }
--- src/main/java/com/mkh/equationapp/service/impl/RootServiceImpl.java
+++ src/main/java/com/mkh/equationapp/service/impl/RootServiceImpl.java
@@ -1,6 +1,8 @@
 package com.mkh.equationapp.service.impl;
 
 import com.mkh.equationapp.service.RootService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RootServiceImpl implements RootService {
 }
