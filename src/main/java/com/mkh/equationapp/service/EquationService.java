273b6a8dd5a6d4e23e2317e957c4f7478f1c046a
mykola <mykola.khlopyk@gmail.com>
2023-06-15 23:17:40 +0300

    implemented methods getAll, getEquationById, deleteRoot in EquationService

--- src/main/java/com/mkh/equationapp/service/EquationService.java
+++ src/main/java/com/mkh/equationapp/service/EquationService.java
@@ -2,6 +2,14 @@ package com.mkh.equationapp.service;
 
 import com.mkh.equationapp.domain.Equation;
 
+import java.util.List;
+
 public interface EquationService {
     void save(Equation equation);
+
+    List<Equation> getAll();
+
+    Equation getEquationById(long id);
+
+    void deleteRoot(long root_id);
 }
--- src/main/java/com/mkh/equationapp/service/impl/EquationServiceImpl.java
+++ src/main/java/com/mkh/equationapp/service/impl/EquationServiceImpl.java
@@ -2,16 +2,42 @@ package com.mkh.equationapp.service.impl;
 
 import com.mkh.equationapp.domain.Equation;
 import com.mkh.equationapp.repository.EquationRepository;
+import com.mkh.equationapp.repository.RootRepository;
 import com.mkh.equationapp.service.EquationService;
 import lombok.AllArgsConstructor;
+import org.hibernate.Hibernate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
+@Transactional(readOnly = true)
 public class EquationServiceImpl implements EquationService {
     private final EquationRepository equationRepository;
+    private final RootRepository rootRepository;
     @Override
+    @Transactional
     public void save(Equation equation) {
         equationRepository.save(equation);
+    }
+
+    @Override
+    public List<Equation> getAll() {
+        return equationRepository.findAll();
+    }
+
+    @Override
+    public Equation getEquationById(long id) {
+        Equation equation = equationRepository.getReferenceById(id);
+        Hibernate.initialize(equation.getRoots());
+        return equation;
+    }
+
+    @Override
+    @Transactional
+    public void deleteRoot(long root_id) {
+        rootRepository.deleteById(root_id);
     }
 }
